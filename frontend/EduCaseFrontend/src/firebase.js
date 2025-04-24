f4abed684ab8f44b81a48f45cfe15c9a7906e8d1
JRDNVI <20096529.wit@gmail.com>
2025-04-24 06:01:17 +0100

    Setup firebase hosting

---
+++ frontend/EduCaseFrontend/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHlD3k_I2ehkPCb9LodBwh995-lk8SSSs",
+  authDomain: "educase-jc.firebaseapp.com",
+  projectId: "educase-jc",
+  storageBucket: "educase-jc.firebasestorage.app",
+  messagingSenderId: "269868168825",
+  appId: "1:269868168825:web:e5606c34e85c9b981bc6a8",
+  measurementId: "G-L5X9SR9J74"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); // optional
+
+export default app;
